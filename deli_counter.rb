fdb21ec82b3bc6d736b226f462e512bfb1cf7f64
Scott Weinstein <sweinstein27@gmail.com>
2018-04-26 14:24:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,3 +11,14 @@ def take_a_number(katz_deli, name)
   position = katz_deli.find_index("#{name}") + 1
   puts "Welcome, #{name}. You are number #{position} in line."
 end
+
+def now_serving(katz_deli)
+  next_in_line = katz_deli.first
+  if katz_deli == []
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{next_in_line}"
+    katz_deli.shift
+  end
+
+    
