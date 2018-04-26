f988928f653d0b0e1bbd8407d507eaa166e0fd84
Scott Weinstein <sweinstein27@gmail.com>
2018-04-26 14:18:30 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,5 +9,5 @@ end
 def take_a_number(katz_deli, name)
   katz_deli << name
   position = katz_deli.find_index("#{name}") + 1
-  puts "Welcome, #{name}. You are #{position} in line."
+  puts "Welcome, #{name}. You are number #{position} in line."
 end
