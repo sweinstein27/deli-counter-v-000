978ecba2bc4d69da75c0e05d0dad2145a68695cf
Scott Weinstein <sweinstein27@gmail.com>
2018-04-26 14:06:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
-katz_deli = [scott, sam]
+katz_deli = ["scott", "sam"]
 
 def line(katz_deli)
   katz_deli.each_with_index do  |value, index|
-    puts "#{index}. #{value}"
+    puts "#{index + 1}. #{value}"
   end
 end
